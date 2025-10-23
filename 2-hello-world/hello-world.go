c4a2a36c291537138515feb14c39685c19c5fdd8
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2025-10-23 20:06:38 -0300

    feat: refactor the function

--- 2-hello-world/hello-world.go
+++ 2-hello-world/hello-world.go
@@ -8,31 +8,23 @@ const (
 	idiomaEspanhol   = "espanhol"
 )
 
-const prefixos = map[string]string{
+var prefixos = map[string]string{
 	idiomaPortugues: "Olá, ",
 	idiomaIngles:    "Hello, ",
 	idiomaEspanhol:  "Hola, ",
 }
 
-// const ( 
-// 	prefixoOlaPortugues = "Olá, "
-// 	prefixoOlaIngles    = "Hello, "
-// 	prefixoOlaEspanhol  = "Hola, "
-// )
 
 func Ola(nome string, idioma string) string {
 	if nome == "" {
 		nome = "mundo"
 	}
 
-	switch idioma {
-		case idiomaIngles:
-			return prefixoOlaIngles + nome
-		case idiomaEspanhol:
-			return prefixoOlaEspanhol + nome
-		case idiomaPortugues:
-			return prefixoOlaPortugues + nome
-		default:
-			return prefixoOlaPortugues + nome
+	prefixo, existe := prefixos[idioma]
+
+	if !existe {
+		prefixo = prefixos[idiomaPortugues]
 	}
+
+	return prefixo + nome
 }
