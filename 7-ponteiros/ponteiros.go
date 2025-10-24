e3b4e039e0f4747c5994cadabc51d77023a68b7d
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2025-10-24 08:54:11 -0300

    feat: create testing and erros assert

--- 7-ponteiros/ponteiros.go
+++ 7-ponteiros/ponteiros.go
@@ -1,7 +1,18 @@
 package ponteiros
 
+import (
+	"fmt"
+)
+
+var ErroSaldoInsuficiente = fmt.Errorf("valor maior que saldo disponivel")
+
+type Bitcoin int
 type Carteira struct {
 	saldo int
+}
+
+func NewCarteira(saldo int) *Carteira {
+	return &Carteira{saldo: saldo}
 }
 
 func (c *Carteira) Depositar(valor int) {
@@ -10,4 +21,46 @@ func (c *Carteira) Depositar(valor int) {
 
 func (c *Carteira) Saldo() int {
 	return c.saldo
+}
+
+func (c *Carteira) Retirar(valor int) error {
+	if valor > c.saldo {
+		return ErroSaldoInsuficiente
+	}
+
+	c.saldo -= valor
+	return nil
+}
+
+type CarteiraBitcoin struct {
+	saldo Bitcoin
+}
+
+func NewBitcoinCarteira(saldo Bitcoin) *CarteiraBitcoin {
+	return &CarteiraBitcoin{saldo: saldo}
+}
+
+func (c *CarteiraBitcoin) Depositar(valor Bitcoin) {
+	c.saldo += valor
+}
+
+func (c *CarteiraBitcoin) Saldo() Bitcoin {
+	return c.saldo
+}
+
+func (c *CarteiraBitcoin) Retirar(valor Bitcoin) error {
+	if valor > c.saldo {
+		return ErroSaldoInsuficiente
+	}
+
+	c.saldo -= valor
+	return nil
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
--- 7-ponteiros/ponteiros_test.go
+++ 7-ponteiros/ponteiros_test.go
@@ -5,15 +5,114 @@ import (
 	"testing"
 )
 
+func confirmaErro(t testing.TB, resultado error, esperado string) {
+	t.Helper()
+	if resultado == nil {
+		t.Fatalf("esperava um erro mas não ocorreu nenhum")
+	}
+
+	if resultado.Error() != esperado {
+		t.Errorf("resultado %q, esperado %q", resultado.Error(), esperado)
+	}
+}
+
+func confirmaErroInexistente(t testing.TB, resultado error) {
+	t.Helper()
+	if resultado != nil {
+		t.Fatalf("não esperava um erro, mas ocorreu um: %q", resultado)
+	}
+}
+
 func TestCarteira(t *testing.T) {
 		t.Run("Depositar dinheiro na carteira", func(t *testing.T) {
-				carteira := ponteiros.Carteira{}
+				carteira := ponteiros.NewCarteira(10)
 				carteira.Depositar(10)
 
 				resultado := carteira.Saldo()
-				esperado := 10
+				esperado := 20
+
+				t.Logf("resultado %d, esperado %d", resultado, esperado)
 				if resultado != esperado {
 						t.Errorf("resultado %d, esperado %d", resultado, esperado)
 				}
 		})
-	}
+}
+
+func TestCarteiraBitcoin(t *testing.T) {
+		t.Run("Depositar Bitcoin na carteira", func(t *testing.T) {
+				carteira := ponteiros.NewBitcoinCarteira(ponteiros.Bitcoin(20))
+				carteira.Depositar(ponteiros.Bitcoin(15))
+
+				resultado := carteira.Saldo()
+				esperado := ponteiros.Bitcoin(35)
+
+				t.Logf("resultado %s, esperado %s", resultado, esperado)
+				if resultado != esperado {
+						t.Errorf("resultado %s, esperado %s", resultado, esperado)
+				}
+		})
+}
+
+func TestCarteirasRetirar(t *testing.T) {
+		t.Run("Retirar dinheiro da carteira", func(t *testing.T) {
+				carteira := ponteiros.NewCarteira(50)
+				erro := carteira.Retirar(20)
+
+				resultado := carteira.Saldo()
+				esperado := 30
+
+				t.Logf("resultado %d, esperado %d", resultado, esperado)
+
+				if resultado != esperado {
+						t.Errorf("resultado %d, esperado %d", resultado, esperado)
+				}
+
+				confirmaErroInexistente(t, erro)
+		})
+
+		t.Run("Retirar mais do que o saldo disponível", func(t *testing.T) {
+				carteira := ponteiros.NewCarteira(10)
+				erro := carteira.Retirar(20)
+
+				resultado := carteira.Saldo()
+				esperado := 10
+
+				t.Logf("resultado %d, esperado %d", resultado, esperado)
+
+				confirmaErro(t, erro, ponteiros.ErroSaldoInsuficiente.Error())
+
+				if resultado != esperado || erro == nil {
+						t.Errorf("resultado %d, esperado %d", resultado, esperado)
+				}
+		})
+
+		t.Run("Retirar Bitcoin da carteira", func(t *testing.T) {
+				carteira := ponteiros.NewBitcoinCarteira(ponteiros.Bitcoin(50))
+				erro := carteira.Retirar(ponteiros.Bitcoin(20))
+
+				resultado := carteira.Saldo()
+				esperado := ponteiros.Bitcoin(30)
+
+				t.Logf("resultado %s, esperado %s", resultado, esperado)
+				if resultado != esperado {
+						t.Errorf("resultado %s, esperado %s", resultado, esperado)
+				}
+				confirmaErroInexistente(t, erro)
+		})
+
+		t.Run("Retirar mais Bitcoin do que o saldo disponível", func(t *testing.T) {
+				carteira := ponteiros.NewBitcoinCarteira(ponteiros.Bitcoin(10))
+				erro := carteira.Retirar(ponteiros.Bitcoin(20))
+
+				resultado := carteira.Saldo()
+				esperado := ponteiros.Bitcoin(10)
+
+				t.Logf("resultado %s, esperado %s", resultado, esperado)
+
+				confirmaErro(t, erro, ponteiros.ErroSaldoInsuficiente.Error())
+
+				if resultado != esperado || erro == nil {
+						t.Errorf("resultado %s, esperado %s", resultado, esperado)
+				}
+		})
+}
