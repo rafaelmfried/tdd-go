c07101eaa2af38d909158d740c0e699b7a3485d8
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2025-10-24 11:02:33 -0300

    feat: create http server example to io.writer

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"net/http"
 	"os"
 	dependencias "tdd/9-dependencias"
 )
 
+func HandlerCumprimenta(w http.ResponseWriter, r *http.Request) {
+	dependencias.Cumprimenta(w, "Rafael")
+}
+
 func main() {
+	err := http.ListenAndServe(":3456", http.HandlerFunc(HandlerCumprimenta))
+
+	if err != nil {
+		panic(err)
+	}
+
 	dependencias.Cumprimenta(os.Stdout, "Rafael")
 }
