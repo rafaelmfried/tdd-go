8dc34baca66c902a332be71d6b928409d9524b37
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2025-10-24 19:05:52 -0300

    feat: learning mutex of sync and the difference

--- 14-sync/sync.go
+++ 14-sync/sync.go
@@ -1,17 +1,27 @@
+/*
+go vet para ajudar a revelar problemas comuns em codigo concorrente
+*/
 package sync
+
+import "sync"
 
 func NovoContador() *Contador {
 	return &Contador{}
 }
 
 type Contador struct {
+	sync.Mutex
 	valor int
 }
 
 func (c *Contador) Incrementar() {
+	c.Lock()
+	defer c.Unlock()
 	c.valor++
 }
 
 func (c *Contador) Valor() int {
+	c.Lock()
+	defer c.Unlock()
 	return c.valor
 }
--- 14-sync/sync_test.go
+++ 14-sync/sync_test.go
@@ -1,23 +1,48 @@
 package sync_test
 
 import (
-	sync "tdd/14-sync"
+	"sync"
+	. "tdd/14-sync"
 	"testing"
 )
 
 func TestContador(t *testing.T) {
 	t.Run("incrementar o contador 3 vezes deve resultar em 3", func(t *testing.T) {
-		contador := sync.NovoContador()
+		contador := NovoContador()
 
 		contador.Incrementar()
 		contador.Incrementar()
 		contador.Incrementar()
-
-		resultado := contador.Valor()
-		valorEsperado := 3
-
-		if resultado != valorEsperado {
-			t.Errorf("resultado %d, valor esperado %d", resultado, valorEsperado)
-		}
+		verificarContador(t, contador, 3)
 	})
+
+	t.Run("roda concorrentement em seguranca", func(t *testing.T) {
+		contador := NovoContador()
+		const totalDeRoutines = 100
+		const incrementosPorRoutine = 1000
+		// Maneira simples de esperar todas as goroutines terminarem
+		var wg sync.WaitGroup
+		wg.Add(totalDeRoutines)
+
+		for i := 0; i < totalDeRoutines; i++ {
+			go func() {
+				for j := 0; j < incrementosPorRoutine; j++ {
+					contador.Incrementar()
+				}
+				wg.Done()
+			}()
+		}
+
+		wg.Wait()
+		verificarContador(t, contador, totalDeRoutines*incrementosPorRoutine)
+	})
+}
+
+func verificarContador(t *testing.T, contador *Contador, valorEsperado int) {
+	t.Helper()
+	resultado := contador.Valor()
+
+	if resultado != valorEsperado {
+		t.Errorf("resultado %d, valor esperado %d", resultado, valorEsperado)
+	}
 }
