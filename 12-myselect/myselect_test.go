fa4e963d4ead9b5323f8e76b2b754a2fd27fe0a0
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2025-10-24 16:31:16 -0300

    feat: using net/http/test

--- 12-myselect/myselect_test.go
+++ 12-myselect/myselect_test.go
@@ -1,14 +1,31 @@
+/*
+	Aqui a gente usou a biblioteca net/http/httptest para criar servidores HTTP de teste.
+	Esses servidores simulam respostas lentas e rápidas para testar a função Corredor.
+*/
+
 package myselect_test
 
 import (
+	"net/http"
+	"net/http/httptest"
 	myselect "tdd/12-myselect"
 	"testing"
+	"time"
 )
 
 func TestCorredor(t *testing.T) {
 	t.Run("teste de concorrencia com select", func(t *testing.T) {
-		URLLenta := "http://www.facebook.com"
-		URLRapida := "http://www.quii.co.uk"
+		servidorLento := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(20 * time.Millisecond)
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		servidorRapido := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		URLLenta := servidorLento.URL
+		URLRapida := servidorRapido.URL
 
 		esperado := URLRapida
 
@@ -17,5 +34,8 @@ func TestCorredor(t *testing.T) {
 		if obtido != esperado {
 			t.Errorf("obtido: %q, esperado: %q", obtido, esperado)
 		}
+
+		servidorLento.Close()
+		servidorRapido.Close()
 	})
 }
