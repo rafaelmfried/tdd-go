8dde9c79a501c987eaba71decdf429eff1920239
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2025-10-29 21:18:21 -0300

    feat: delete some files modifi the main to run the web app and remove git ignore

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 	dependencias "tdd/9-dependencias"
+	"tdd/application/1-http/linha-de-comando/armazenamento"
+	"tdd/application/1-http/linha-de-comando/servidor"
 )
 
 func HandlerCumprimenta(w http.ResponseWriter, r *http.Request) {
@@ -10,10 +14,18 @@ func HandlerCumprimenta(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// armazenamento := server.NovoArmazenamentoJogadorDoArquivo()
-	// server := server.NewServidorJogador(armazenamento)
+	db, err := os.OpenFile("jogo.db.json", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("opening db file %v", err)
+	}
+	defer db.Close()
+	store, err := armazenamento.NovoArmazenamentoJogadorDoArquivo(db)
+	if err != nil {
+		log.Fatalf("creating player storage %v", err)
+	}
+	server := servidor.NewServidorJogador(store)
 
-	// if err := http.ListenAndServe(":2312", server); err != nil {
-	// 	log.Fatalf("binding port 2312 %v", err)
-	// }
+	if err := http.ListenAndServe(":2312", server); err != nil {
+		log.Fatalf("binding port 2312 %v", err)
+	}
 }
